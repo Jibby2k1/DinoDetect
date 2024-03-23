fed246eb64486467fc7c2c2fa9a08bf541b429c9
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-03-23 10:26:54 -0400

    env envariables

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('dotenv').config();
+// Use env vars like so "process.env.DISCORD_API_KEY"
+
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login('your-token-goes-here');
+client.login();
